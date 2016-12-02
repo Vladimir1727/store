018d83f92f453c4dca0a15e916708be3d709af98
vladimir <dvn125@gmail.com>
2016-12-02 20:16:07 +0200

    home 021216

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
 (function($){$(function(){
 
-
 $("#tabs").tabs();
+
+$.fn.show_err=function(){//функция подсветки пустых полей
+var test=this;
+	test.blur(function(){
+		test.val(test.val().trim());
+		if (test.val().trim().length<1)	test.css({"border":"1px red solid"})
+		else test.css({"border":"1px #ccc solid"});	
+	});
+}
+
+$.fn.try_add=function(arr){//функция проверки на пустые поля при нажатии кнопки
+var butt=this;
+	butt.click(function(){
+		for (var i = arr.length - 1; i >= 0; i--) {
+			if (arr[i].val().trim().length<1 || arr[i].val().trim()=="0"){
+				console.log('error');
+				return false;
+			}
+		}
+	return true;
+	});
+}
+
+//вкладка категорий
+$('#input_cat').show_err();
+$('#add_cat').try_add([$('#input_cat')]);
+$('#input_sub').show_err();
+$('#add_sub').try_add([$('#input_sub'),$('#sel_cat1')]);
+//вкладка товара
+$('#itemname').show_err();
+$('#pricein').show_err();
+$('#pricesale').show_err();
+$('#info').show_err();
+$('#add_item').try_add([$('#itemname'),$('#pricein'),$('#pricesale'),$('#info'),$('#sel_cat2'),$('#subid'),$('#itempic')]);
+//вкладка картинки
+
+$('#addpics').try_add([$('#sel_cat3'),$('#subid2'),$('#itemlist'),$('#files')]);
 
 })})(jQuery)
--- pages/admin.php
+++ pages/admin.php
@@ -8,8 +8,8 @@
     <div class="col-md-6 col-sm-12">
 	    <h3>Категории</h3>
 		<form action="index.php?page=5" method="post" class="form-inline">
-			<input type="text" placeholder="категория..." name="newcat"  class="form-control">
-			<input type="submit" value="Добавить" name="addcat" class="btn btn-default">
+			<input type="text" placeholder="категория..." name="newcat"  class="form-control"  id="input_cat">
+			<input type="submit" value="Добавить" name="addcat" class="btn btn-default" id="add_cat">
 		</form>
 		<?php 
 		if (isset($_POST['addcat'])){
@@ -25,7 +25,7 @@
 	<div class="col-md-6 col-sm-12">
     	<h3>Подкатегории</h3>
     	<form action="index.php?page=5" method="post" class="form-inline">
-    		<select name="cat">
+    		<select name="cat" id="sel_cat1">
     			<option value="0">выберите категорию</option>
     			<?php 
     			$sel=$pdo->prepare('select * from categories');
@@ -36,8 +36,8 @@
 				}
     			?>
     		</select>
-    		<input  type="text" placeholder="подкатегория..." name="newsub" class="form-control">
-    		<input type="submit" value="Добавить" name="addsub" class="btn btn-default">
+    		<input  type="text" placeholder="подкатегория..." name="newsub" class="form-control" id="input_sub">
+    		<input type="submit" value="Добавить" name="addsub" class="btn btn-default" id="add_sub">
     	</form>
     	<?php 
     	if (isset($_POST['addsub'])){
@@ -56,7 +56,7 @@
     <h3>Добавить товар</h3>
     <form action="index.php?page=5#fragment-2" method="post" class="input-group"  enctype="multipart/form-data">
     	<div class="input-group">
-    	<select name="catid" id="" onchange="showSC(this.value)">
+    	<select name="catid" id="sel_cat2" onchange="showSC(this.value)">
 	    <option value="0">Выберите категорию</option>
 	    	<?php 
 	    		$sel=$pdo->prepare('select * from categories');
@@ -76,7 +76,7 @@
     		<label for="pricein" class="input-group-addon">Цена покупки:</label>
     		<input type="text" placeholder="цена покупки..." name="pricein" id="pricein" class="form-control">
     		<label for="pricesale" class="input-group-addon">Цена продажи:</label>
-    	<input type="text" placeholder="цена продажи..." name="pricesale" id="pricesale" class="form-control">
+    		<input type="text" placeholder="цена продажи..." name="pricesale" id="pricesale" class="form-control">
     	</div>
     	<div class="input-group">
 	    	<label for="itempic" class="input-group-addon">Главная картинка товара:</label>
@@ -84,7 +84,7 @@
     	</div>
     	<textarea name="info" id="info" placeholder="описание..." class="form-control">
 		</textarea>
-		<input type="submit" name="additem" value="Добавить" class="btn btn-default">
+		<input type="submit" name="additem" value="Добавить" class="btn btn-default" id="add_item">
 		</form>
 	<?php 
     	if (isset($_POST['additem'])){
@@ -110,7 +110,7 @@
   	<h3>Добавить картинки к товару</h3>
     <form action="index.php?page=5#fragment-3" method="post" class="input-group"  enctype="multipart/form-data">
     	<div class="input-group">
-    		<select name="catid" id="" onchange="showSC2(this.value)">
+    		<select name="catid" id="sel_cat3" onchange="showSC2(this.value)">
     		<option value="0">Выберите категорию</option>
 	    		<?php 
 	    			$sel=$pdo->prepare('select * from categories');
@@ -126,7 +126,7 @@
 		<div class="input-group">
 			<input type="file" name="file[]" multiple accept="image/*" class="form-control"  id="files">
     	</div>
-    	<input type="submit" name="addpics" value="Добавить" class="btn btn-default">
+    	<input type="submit" name="addpics" value="Добавить" class="btn btn-default" id="addpics">
     </form>
 	<?php 
     	if (isset($_POST['addpics']) && $_POST['itemlist']>0){
