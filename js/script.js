8646d00d597c64fd98ae6947b8c218570a07e2b2
vladimir <dvn125@gmail.com>
2016-12-03 07:02:41 +0200

    home 03/12/16

--- js/script.js
+++ js/script.js
@@ -2,44 +2,38 @@
 
 $("#tabs").tabs();
 
-$.fn.show_err=function(){//функция подсветки пустых полей
+/*$.fn.show_err=function(){//функция подсветки пустых полей
 var test=this;
 	test.blur(function(){
 		test.val(test.val().trim());
 		if (test.val().trim().length<1)	test.css({"border":"1px red solid"})
 		else test.css({"border":"1px #ccc solid"});	
 	});
-}
+}*/
 
 $.fn.try_add=function(arr){//функция проверки на пустые поля при нажатии кнопки
 var butt=this;
 	butt.click(function(){
 		for (var i = arr.length - 1; i >= 0; i--) {
-			if (arr[i].val().trim().length<1 || arr[i].val().trim()=="0"){
+			test=$("#"+arr[i]);
+			if (test.val().trim().length<1 || test.val().trim()=="0"){
+				test.css({"border":"1px red solid"})
 				console.log('error');
 				return false;
 			}
+			else test.css({"border":"1px #ccc solid"});
 		}
 	return true;
 	});
 }
 
 //вкладка категорий
-$('#input_cat').show_err();
-$('#add_cat').try_add([$('#input_cat')]);
-$('#input_sub').show_err();
-$('#add_sub').try_add([$('#input_sub'),$('#sel_cat1')]);
+$('#add_cat').try_add(['input_cat']);
+$('#add_sub').try_add(['input_sub','sel_cat1']);
 //вкладка товара
-$('#itemname').show_err();
-$('#pricein').show_err();
-$('#pricesale').show_err();
-$('#info').show_err();
-$('#add_item').try_add([$('#itemname'),$('#pricein'),$('#pricesale'),$('#info'),$('#sel_cat2'),$('#subid'),$('#itempic')]);
+$('#add_item').try_add(['itemname','pricein','pricesale','info','sel_cat2','subid','itempic']);
 //вкладка картинки
-$('#addpics').try_add([$('#sel_cat3'),$('#subid2'),$('#itemlist'),$('#files')]);
+$('#addpics').try_add(['sel_cat3','subid2','itemlist','files']);
 //регистрация
-$('#pass1').show_err();
-$('#pass2').show_err();
-$('#login').show_err();
-$('#adduser').try_add([$('#pass1'),$('#pass2'),$('#login'),$('#file_pic')]);
+$('#adduser').try_add(['pass1','pass2','login','file_pic']);
 })})(jQuery)
